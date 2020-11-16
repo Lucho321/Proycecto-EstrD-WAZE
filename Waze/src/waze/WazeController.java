6269f2dc97f83acc600a438ec8361fc4bab87c56
Jeffry99 <54681342+Jeffry99@users.noreply.github.com>
2020-11-15 21:15:09 -0600

    Si pu

--- Waze/src/waze/WazeController.java
+++ Waze/src/waze/WazeController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -33,15 +34,16 @@ public class WazeController implements Initializable {
     @FXML
     private ImageView mapa;
     @FXML
-    private CheckBox btnlineas;
+    private CheckBox btnVertices;
     @FXML
-    private CheckBox btnVerices;
+    private CheckBox btnlineas;
     @FXML
     private Circle v33, v8, v9, v34, v10, v46, v48, v45, v35, v36, v32, v43, v11, v37, v39, v38, 
             v49, v7, v42, v54, v50, v65, v64, v6, v66, v30, v5, v4, v3, v29, v28, v2, v1, v12,
             v31, v41, v27, v26, v44, v47, v63, v72, v57, v55, v25, v60, v58, v59, v24, v61, v84,
             v62, v73, v74, v53, v83, v51, v40, v13, v14, v69, v52, v68, v67, v71, v76, v75, v23,
-            v82, v21, v22, v20, v81, v80, v70, v77, v19, v79, v78, v18, v15, v16, v17, v56;
+            v82, v21, v22, v20, v81, v80, v70, v77, v19, v79, v78, v18, v15, v16, v17, v56, v85, 
+            v86, v87, v88, v89, v90, v91;
     @FXML
     private Line l24, l22, l21, l20, l19, l18, l14, l17, l15, l26, l25, l8, l7, l6, l5, l4, l3, l2, l1,
             l27, l11, l12, l10, l9, l13, l16, l30, l29, l28, l38, l35, l36, l31, l32, l33, l34, l42,
@@ -79,23 +81,17 @@ public class WazeController implements Initializable {
     private Line l1261111;
     @FXML
     private Line l126111;
-    @FXML
-    private Circle v110;
-    @FXML
-    private Line line128;
-    private Circle v112;
-    @FXML
-    private Circle v1121;
-    private Circle v1122;
-    @FXML
-    private Circle v111;
-    @FXML
-    private Circle v11211;
     
-    private Circle v112111;
 
+    private List<Circle> circles = new ArrayList();
+    private List<Circle> vertices = new ArrayList();
+    @FXML
+    private Label lblPunto;
+    @FXML
+    private Button btnCancelar;
     
-    private List<Vertice> vertices = new ArrayList();
+    private String modo = "";
+    
     
     /**
      * Initializes the controller class.
@@ -103,6 +99,8 @@ public class WazeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        addVertices();
+        
         Image image = new Image(getClass().getResourceAsStream("/imagenes/choque.png"));
         ImageView imgView = new ImageView(image);
         imgView.setFitHeight(50);
@@ -122,9 +120,12 @@ public class WazeController implements Initializable {
         btnCerrarCalle.setGraphic(imgView);
         
         btnlineas.setSelected(true);
-        btnVerices.setSelected(true);
+        btnVertices.setSelected(true);
         setLineasVisible();
         setVerticesVisible();
+        
+        lblPunto.setVisible(false);
+        btnCancelar.setVisible(false);
     }    
 
     @FXML
@@ -135,13 +136,38 @@ public class WazeController implements Initializable {
     @FXML
     private void clickCircle(MouseEvent event) {
         Circle c = (Circle)event.getSource();
-        c.setFill(Color.ORANGE);
         
-    }
+        
+        if(modo.equals("seleccionarRuta")){
+            modo = "puntoInicio";
+            vertices.add(c);
+            c.setFill(Color.ORANGE);
+            lblPunto.setText("Seleccione el punto de llegada");
+            return;
+        }
+        if(modo.equals("puntoInicio")){
+            //vertices.get(0).setFill(Color.WHITE);
+            //vertices.remove(0);
+            modo = "puntoLlegada";
+            vertices.add(c);
+            c.setFill(Color.ORANGE);
+            lblPunto.setText("");
+            return;
+        }
+//        if(modo.equals("puntoLlegada")){
+//            vertices.get(1).setFill(Color.WHITE);
+//            vertices.remove(1);
+//            vertices.add(c);
+//            modo = "puntoLlegada";
+//        }
+    }  
+    
     @FXML
     private void actLineas(ActionEvent event) {
         setLineasVisible();
     } 
+    
+    boolean verticesVisibles;
     @FXML
     private void actVertices(ActionEvent event) {
         setVerticesVisible();
@@ -273,91 +299,13 @@ public class WazeController implements Initializable {
         l125.setVisible(btnlineas.isSelected());
         l128.setVisible(btnlineas.isSelected());
     }
+    
     private void setVerticesVisible(){
-        v1.setVisible(btnVerices.isSelected());
-        v2.setVisible(btnVerices.isSelected());
-        v3.setVisible(btnVerices.isSelected());
-        v4.setVisible(btnVerices.isSelected());
-        v5.setVisible(btnVerices.isSelected());
-        v6.setVisible(btnVerices.isSelected());
-        v7.setVisible(btnVerices.isSelected());
-        v8.setVisible(btnVerices.isSelected());
-        v9.setVisible(btnVerices.isSelected());
-        v10.setVisible(btnVerices.isSelected());
-        v11.setVisible(btnVerices.isSelected());
-        v12.setVisible(btnVerices.isSelected());
-        v13.setVisible(btnVerices.isSelected());
-        v14.setVisible(btnVerices.isSelected());
-        v15.setVisible(btnVerices.isSelected());
-        v16.setVisible(btnVerices.isSelected());
-        v17.setVisible(btnVerices.isSelected());
-        v18.setVisible(btnVerices.isSelected());
-        v19.setVisible(btnVerices.isSelected());
-        v20.setVisible(btnVerices.isSelected());
-        v21.setVisible(btnVerices.isSelected());
-        v22.setVisible(btnVerices.isSelected());
-        v23.setVisible(btnVerices.isSelected());
-        v24.setVisible(btnVerices.isSelected());
-        v25.setVisible(btnVerices.isSelected());
-        v26.setVisible(btnVerices.isSelected());
-        v27.setVisible(btnVerices.isSelected());
-        v28.setVisible(btnVerices.isSelected());
-        v29.setVisible(btnVerices.isSelected());
-        v30.setVisible(btnVerices.isSelected());
-        v31.setVisible(btnVerices.isSelected());
-        v32.setVisible(btnVerices.isSelected());
-        v33.setVisible(btnVerices.isSelected());
-        v34.setVisible(btnVerices.isSelected());
-        v35.setVisible(btnVerices.isSelected());
-        v36.setVisible(btnVerices.isSelected());
-        v37.setVisible(btnVerices.isSelected());
-        v38.setVisible(btnVerices.isSelected());
-        v39.setVisible(btnVerices.isSelected());
-        v40.setVisible(btnVerices.isSelected());
-        v41.setVisible(btnVerices.isSelected());
-        v42.setVisible(btnVerices.isSelected());
-        v43.setVisible(btnVerices.isSelected());
-        v44.setVisible(btnVerices.isSelected());
-        v45.setVisible(btnVerices.isSelected());
-        v46.setVisible(btnVerices.isSelected());
-        v47.setVisible(btnVerices.isSelected());
-        v48.setVisible(btnVerices.isSelected());
-        v49.setVisible(btnVerices.isSelected());
-        v50.setVisible(btnVerices.isSelected());
-        v51.setVisible(btnVerices.isSelected());
-        v52.setVisible(btnVerices.isSelected());
-        v53.setVisible(btnVerices.isSelected());
-        v54.setVisible(btnVerices.isSelected());
-        v55.setVisible(btnVerices.isSelected());
-        v56.setVisible(btnVerices.isSelected());
-        v57.setVisible(btnVerices.isSelected());
-        v58.setVisible(btnVerices.isSelected());
-        v59.setVisible(btnVerices.isSelected());
-        v60.setVisible(btnVerices.isSelected());
-        v61.setVisible(btnVerices.isSelected());
-        v62.setVisible(btnVerices.isSelected());
-        v63.setVisible(btnVerices.isSelected());
-        v64.setVisible(btnVerices.isSelected());
-        v65.setVisible(btnVerices.isSelected());
-        v66.setVisible(btnVerices.isSelected());
-        v67.setVisible(btnVerices.isSelected());
-        v68.setVisible(btnVerices.isSelected());
-        v69.setVisible(btnVerices.isSelected());
-        v70.setVisible(btnVerices.isSelected());
-        v71.setVisible(btnVerices.isSelected());
-        v72.setVisible(btnVerices.isSelected());
-        v73.setVisible(btnVerices.isSelected());
-        v74.setVisible(btnVerices.isSelected());
-        v75.setVisible(btnVerices.isSelected());
-        v76.setVisible(btnVerices.isSelected());
-        v77.setVisible(btnVerices.isSelected());
-        v78.setVisible(btnVerices.isSelected());
-        v79.setVisible(btnVerices.isSelected());
-        v80.setVisible(btnVerices.isSelected());
-        v81.setVisible(btnVerices.isSelected());
-        v82.setVisible(btnVerices.isSelected());
-        v83.setVisible(btnVerices.isSelected()); 
-        v84.setVisible(btnVerices.isSelected());
+        circles.forEach(x->{
+            x.setVisible(btnVertices.isSelected());
+        });
+        lblPunto.setText("Seleccione el punto de llegada");
+        btnCancelar.setVisible(true);
     }
 
     @FXML
@@ -375,12 +323,64 @@ public class WazeController implements Initializable {
     @FXML
     private void actDijkstra(ActionEvent event){
         cbxFloyd.setSelected(false);
+        seleccionarRuta();
     }
     
     @FXML
     private void actFloyd(ActionEvent event){
         cbxDijkstra.setSelected(false);
+        seleccionarRuta();
     }
     
+    private int cont = 0;
+    private void addVertices(){
+
+        circles.add(v1); circles.add(v2); circles.add(v3); circles.add(v4); circles.add(v5); circles.add(v6);
+        circles.add(v7); circles.add(v8); circles.add(v9); circles.add(v10); circles.add(v11); circles.add(v12);
+        circles.add(v13); circles.add(v14); circles.add(v15); circles.add(v16); circles.add(v17); circles.add(v18);
+        circles.add(v19); circles.add(v20); circles.add(v21); circles.add(v22); circles.add(v23); circles.add(v24);
+        circles.add(v25); circles.add(v26); circles.add(v27); circles.add(v28); circles.add(v29); circles.add(v30);
+        circles.add(v31); circles.add(v32); circles.add(v33); circles.add(v34); circles.add(v35); circles.add(v36);
+        circles.add(v37); circles.add(v38); circles.add(v39); circles.add(v40); circles.add(v41); circles.add(v42);
+        circles.add(v43); circles.add(v44); circles.add(v45); circles.add(v46); circles.add(v47); circles.add(v48);
+        circles.add(v49); circles.add(v50); circles.add(v51); circles.add(v52); circles.add(v53); circles.add(v54);
+        circles.add(v55); circles.add(v56); circles.add(v57); circles.add(v58); circles.add(v59); circles.add(v60);
+        circles.add(v61); circles.add(v62); circles.add(v63); circles.add(v64); circles.add(v65); circles.add(v66);
+        circles.add(v67); circles.add(v68); circles.add(v69); circles.add(v70); circles.add(v71); circles.add(v72);
+        circles.add(v73); circles.add(v74); circles.add(v75); circles.add(v76); circles.add(v77); circles.add(v78);
+        circles.add(v79); circles.add(v80); circles.add(v81); circles.add(v82); circles.add(v83); circles.add(v84);
+        circles.add(v85); circles.add(v86); circles.add(v87); circles.add(v88); circles.add(v89); circles.add(v90);
+        circles.add(v91);
+        
+        
+        circles.forEach(x-> {
+            x.setId(String.valueOf(cont));
+            cont++;
+        });
+    }
+
+    @FXML
+    private void actCancelar(ActionEvent event) {
+        if(modo.equals("puntoInicio")){
+            vertices.get(0).setFill(Color.WHITE);
+            vertices.clear();
+            modo = "seleccionarRuta";
+            lblPunto.setText("Seleccione el punto de partida");  
+        }
+        if(modo.equals("puntoLlegada")){
+            if(vertices.get(1) != null){
+                vertices.get(1).setFill(Color.WHITE);
+                vertices.remove(1);
+                modo = "puntoInicio";
+                lblPunto.setText("Seleccione el punto de llegada");
+            }
+        }
+    }
     
+    private void seleccionarRuta(){
+        lblPunto.setVisible(true);
+        lblPunto.setText("Seleccione el punto de partida");
+        btnCancelar.setVisible(true);
+        modo = "seleccionarRuta";
+    }
 }
